0c8cb45f93b8247f3ed871085539d02de61b709c
bingxueshuang <94691555+bingxueshuang@users.noreply.github.com>
2022-02-14 19:42:04 +0530

    Basic structure (#2)
    Include ejs for rendering html.
    Modify a few eslint rules.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const { createServer } = require('http');
-const { PORT } = require('./config')
-let server = createServer( (req, res) => {
-    let { url } = req;
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    res.end(`Hello, welcome to bbms site, you are here at ${url}`);
+const { PORT } = require('./config');
+const { renderFile: ejsRender } = require('ejs');
+
+const server = createServer(async (req, res) => {
+  const { url } = req;
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(await ejsRender('./views/index.ejs', { title:'Title', url }));
 });
 server.listen(PORT);
