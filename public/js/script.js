4712d58a13304c468b4991763666650322c5e858
Manojna <manojnakp.201cs230@nitk.edu.in>
2022-05-02 13:19:08 +0530

    Make small changes and simplifications
    - Fancy 404 page
    - integrate login page with bootstrap
    - integrate staffs page with bootstrap
    - modify header and footer pages
    - modify buttons (edit and delete)

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,3 @@
-const toggleAccordion = function () {
-  const classes = this.parentElement.classList;
-  const body = this.parentElement.querySelector('.accordion-body');
-  if (classes.contains('collapse')) {
-    // expand it
-    const evtListener = () => {
-      body.removeEventListener('transitionend', evtListener);
-      body.style.height = null;
-      classes.remove('collapse');
-    };
-    const { scrollHeight } = body;
-    body.style.height = `${scrollHeight}px`;
-    body.addEventListener('transitionend', evtListener);
-  } else {
-    // collapse it
-    const { scrollHeight } = body;
-    const { transition } = body.style;
-    body.style.transition = '';
-    requestAnimationFrame(() => {
-      body.style.height = `${scrollHeight}px`;
-      body.style.transition = transition;
-
-      requestAnimationFrame(() => {
-        body.style.height = `${0}px`;
-        classes.add('collapse');
-        body.style.height = null;
-      });
-    });
-  }
-};
-
-document.querySelectorAll('.accordion-header').forEach(toggler => {
-  toggler.addEventListener('click', toggleAccordion);
-});
 const alerts = document.querySelectorAll('.alert');
 for (const alert of alerts) {
   alert.querySelector('.btn-close').addEventListener('click', function () {
