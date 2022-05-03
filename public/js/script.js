4ff3e3210cfa003bd20b02af8eae5dafa860803b
Manojna <manojnakp.201cs230@nitk.edu.in>
2022-05-03 12:13:15 +0530

    Alerts and accordion
    Update all pages

--- public/js/script.js
+++
@@ -1,6 +0,0 @@
-const alerts = document.querySelectorAll('.alert');
-for (const alert of alerts) {
-  alert.querySelector('.btn-close').addEventListener('click', function () {
-    this.parentElement.remove();
-  });
-}
